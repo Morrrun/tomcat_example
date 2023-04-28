97f6530a88d43c18e6137a5f674e07ad29aabe08
Morrrun <mifon.199708@gmail.com>
2023-04-28 19:30:10 +0400

    Переопределен метод doGet() Добавлен мапинг в виде аннотации

--- src/main/java/com/alexsandrov/tomcat_example/servlet/FirstServlet.java
+++ src/main/java/com/alexsandrov/tomcat_example/servlet/FirstServlet.java
@@ -2,12 +2,16 @@ package com.alexsandrov.tomcat_example.servlet;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+//Аннотация что-бы связать определенный путь с нашим Servlet-ом
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 
     //Первый этап ЖЦ Servlet
@@ -32,5 +36,11 @@ public class FirstServlet extends HttpServlet {
     }
 
 
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("<h2>Hello from <i>First Servlet</i></h2>");
+        }
+    }
 }
