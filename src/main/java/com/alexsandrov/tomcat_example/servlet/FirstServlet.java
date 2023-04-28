70cdcf7eca51a0fbcc454f11d7d1bf324b2fd42a
Morrrun <mifon.199708@gmail.com>
2023-04-28 19:02:45 +0400

    Init project

---
+++ src/main/java/com/alexsandrov/tomcat_example/servlet/FirstServlet.java
@@ -0,0 +1,36 @@
+package com.alexsandrov.tomcat_example.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+
+    //Первый этап ЖЦ Servlet
+    //(инициализация [загрузка класса Servlet-а, создание объекта Servlet-а, и вызов данного метода])
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+    //Второй этап ЖЦ Servlet
+    //(обработка входящих запросов[requestInitialisation, requestDestroyed])
+    //Не переопределяется, т.к. использует механизм else-if для определения типа HTTP-метода,
+    //вместо него переопределяем такие методы как doGet(), doPost и т.д.
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+    //Третий этап ЖЦ Servlet
+    //(уничтожение работающего экземпляра по завершению работы приложения)
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+
+
+}
